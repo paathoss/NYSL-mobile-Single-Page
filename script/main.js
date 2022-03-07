819e79d069004b1669edbd926464396e097c651d
paathoss <gafasmugrientasdelyo@gmail.com>
2022-03-07 19:23:01 -0300

    Single-Page

---
+++ script/main.js
@@ -0,0 +1,386 @@
+var meses = [{
+  "month_name": "september",
+  "teams": [
+    {
+      "vsName": "U1 and U4",
+      "location": "AJ Katzenmaier",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.7810708171005!2d-87.86499018454475!3d42.32586807918915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880f932fbc6ba7cd%3A0xcf2bbe275c6da815!2sAJ%20Katzenmaier%20Academy!5e0!3m2!1ses!2sar!4v1639062228330!5m2!1ses!2sar",
+      "times": "9:30a.m.",
+      "day": "01"
+    },
+    {
+      "vsName": "U3 and U2",
+      "location": "Greenbay",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.022981686595!2d-87.64002218455776!3d41.913864679219344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3407260c45b%3A0xb351205fae50c6f3!2sGreenbay%20Elementary!5e0!3m2!1ses!2sar!4v1639063309363!5m2!1ses!2sar",
+      "times": "1:00 p.m.",
+      "day": "01"
+    },
+    {
+      "vsName": "U5 and U6",
+      "location": "Howard A Yeager",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5854551088346!2d-87.66512358455748!3d41.92326947921855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward%20A%20Yeager%20Elementary!5e0!3m2!1ses!2sar!4v1639063834164!5m2!1ses!2sar",
+      "times": "9:30a.m.",
+      "day": "08"
+    },
+    {
+      "vsName": "U6 and U1",
+      "location": "Marjorie P Hart",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.289839348977!2d-87.64816138455728!3d41.92962287921805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd30f2637f9d7%3A0xdbff5d5dfcfcfa35!2sMarjorie%20P%20Hart%20Elementary!5e0!3m2!1ses!2sar!4v1639064262847!5m2!1ses!2sar",
+      "times": "1:00 p.m.",
+      "day": "08"
+    },
+    {
+      "vsName": "U2 and U4",
+      "location": "North Elementary",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.3355487036233!2d-87.64831158455799!3d41.90714487921991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd33af14860a5%3A0x5736e62f19086c62!2sNorth%20Elementary!5e0!3m2!1ses!2sar!4v1639064507344!5m2!1ses!2sar",
+      "times": "9:30a.m.",
+      "day": "15"
+    },
+    {
+      "vsName": "U3 and U5	",
+      "location": "AJ Katzenmaier",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.7810708171005!2d-87.86499018454475!3d42.32586807918915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880f932fbc6ba7cd%3A0xcf2bbe275c6da815!2sAJ%20Katzenmaier%20Academy!5e0!3m2!1ses!2sar!4v1639062228330!5m2!1ses!2sar",
+      "times": "1:00 p.m.",
+      "day": "15"
+    },
+    {
+      "vsName": "U1 and U3",
+      "location": "South Elementary",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d30755.42435396396!2d-87.9016356782163!3d42.01187894268649!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fb7ad3c474b63%3A0x93bdf85af2a839ed!2sSOUTH%20ELEMENTARY%20SCHOOL!5e0!3m2!1ses!2sar!4v1639064818498!5m2!1ses!2sar",
+      "times": "9:30a.m.",
+      "day": "22"
+    },
+    {
+      "vsName": "U2 and U6",
+      "location": "Howard A Yeager",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5854551088346!2d-87.66512358455748!3d41.92326947921855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward%20A%20Yeager%20Elementary!5e0!3m2!1ses!2sar!4v1639063834164!5m2!1ses!2sar",
+      "times": "1:00 p.m.",
+      "day": "22"
+    },
+    {
+      "vsName": "U4 and U5",
+      "location": "Greenbay",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.022981686595!2d-87.64002218455776!3d41.913864679219344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3407260c45b%3A0xb351205fae50c6f3!2sGreenbay%20Elementary!5e0!3m2!1ses!2sar!4v1639063309363!5m2!1ses!2sar",
+      "times": "9:30a.m.",
+      "day": "29"
+    },
+  ]
+},
+{
+  "month_name": "october",
+  "teams": [
+    {
+      "vsName": "U2 and U5",
+      "location": "Marjorie P Hart",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.289839348977!2d-87.64816138455728!3d41.92962287921805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd30f2637f9d7%3A0xdbff5d5dfcfcfa35!2sMarjorie%20P%20Hart%20Elementary!5e0!3m2!1ses!2sar!4v1639064262847!5m2!1ses!2sar",
+      "times": "9:30a.m.",
+      "day": "06"
+    },
+    {
+      "vsName": "U1 and U6",
+      "location": "South Elementary",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d30755.42435396396!2d-87.9016356782163!3d42.01187894268649!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fb7ad3c474b63%3A0x93bdf85af2a839ed!2sSOUTH%20ELEMENTARY%20SCHOOL!5e0!3m2!1ses!2sar!4v1639064818498!5m2!1ses!2sar",
+      "times": "1:00 p.m.",
+      "day": "06"
+    },
+    {
+      "vsName": "U3 and U4",
+      "location": "Howard A Yeager",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5854551088346!2d-87.66512358455748!3d41.92326947921855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward%20A%20Yeager%20Elementary!5e0!3m2!1ses!2sar!4v1639063834164!5m2!1ses!2sar",
+      "times": "9:30a.m.",
+      "day": "08"
+    },
+    {
+      "vsName": "U5 and U1",
+      "location": "Greenbay",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.022981686595!2d-87.64002218455776!3d41.913864679219344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3407260c45b%3A0xb351205fae50c6f3!2sGreenbay%20Elementary!5e0!3m2!1ses!2sar!4v1639063309363!5m2!1ses!2sar",
+      "times": "1:00 p.m.",
+      "day": "08"
+    },
+    {
+      "vsName": "U6 and U3",
+      "location": "North Elementary",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.3355487036233!2d-87.64831158455799!3d41.90714487921991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd33af14860a5%3A0x5736e62f19086c62!2sNorth%20Elementary!5e0!3m2!1ses!2sar!4v1639064507344!5m2!1ses!2sar",
+      "times": "9:30a.m.",
+      "day": "20	"
+    },
+    {
+      "vsName": "U2 and U4",
+      "location": "Marjorie P Hart",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.289839348977!2d-87.64816138455728!3d41.92962287921805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd30f2637f9d7%3A0xdbff5d5dfcfcfa35!2sMarjorie%20P%20Hart%20Elementary!5e0!3m2!1ses!2sar!4v1639064262847!5m2!1ses!2sar",
+      "times": "1:00 p.m.",
+      "day": "20	"
+    },
+    {
+      "vsName": "U3 and U1",
+      "location": "AJ Katzenmaier",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.7810708171005!2d-87.86499018454475!3d42.32586807918915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880f932fbc6ba7cd%3A0xcf2bbe275c6da815!2sAJ%20Katzenmaier%20Academy!5e0!3m2!1ses!2sar!4v1639062228330!5m2!1ses!2sar",
+      "times": "9:30a.m.",
+      "day": "27"
+    },
+    {
+      "vsName": "U5 and U6",
+      "location": "Howard A Yeager",
+      "map_iframe": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5854551088346!2d-87.66512358455748!3d41.92326947921855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward%20A%20Yeager%20Elementary!5e0!3m2!1ses!2sar!4v1639063834164!5m2!1ses!2sar",
+      "times": "1:00 p.m.",
+      "day": "27"
+    }
+  ]
+}
+]
+
+function show() {
+  var toggle = document.getElementById('sidebar').classList.toggle('active');
+  var blur = document.getElementById("blur");
+
+  if (toggle = 'active') {
+    document.getElementById('blur').classList.add('blureado');
+    document.getElementById('blur').style.zIndex = 100;
+  }
+
+
+  if (blur.style.display === "block") {
+    blur.style.display = "none";
+  } else {
+    blur.style.display = "block";
+  }
+}
+
+
+const indexP = document.getElementById("indexPage");
+const aboutP = document.getElementById("aboutPage");
+const registrationP = document.getElementById("registrationPage");
+const contactP = document.getElementById("contactPage");
+const infoP = document.getElementById("infoPage");
+const rulesP = document.getElementById("rulesPage");
+
+
+function render(pagina) {
+  document.getElementById('sidebar').classList.toggle('active');
+  document.getElementById('blur').style.display = "none";
+
+  indexP.style.display = "none";
+  aboutP.style.display = "none";
+  rulesP.style.display = "none";
+  registrationP.style.display = "none";
+  infoP.style.display = "none";
+  contactP.style.display = "none";
+
+  if (pagina === 'aboutP') {
+    aboutP.style.display = "block";
+
+    document.getElementById('sidebar').classList.add('sidebarA');
+    document.getElementById('blur').classList.add('blurA');
+  } else if (pagina === 'registrationP') {
+    registrationP.style.display = "block";
+
+    document.getElementById('sidebar').classList.add('sidebarRe');
+    document.getElementById('blur').classList.add('blurRe');
+  } else if (pagina === 'indexP') {
+    indexP.style.display = "block";
+
+    document.getElementById('sidebar').classList.add('sidebarIn');
+    document.getElementById('blur').classList.add('blureado');
+  } else if (pagina === 'infoP') {
+    infoP.style.display = "block";
+
+    document.getElementById('sidebar').classList.add('sidebarTa');
+    document.getElementById('blur').classList.add('blurTa');
+  } else if (pagina === 'contactP') {
+    contactP.style.display = "block";
+
+    document.getElementById('sidebar').classList.add('sidebarC');
+    document.getElementById('blur').classList.add('blurC');
+  } else if (pagina === 'rulesP') {
+    rulesP.style.display = "block";
+
+    document.getElementById('sidebar').classList.add('sidebarR');
+    document.getElementById('blur').classList.add('blurR');
+  }
+}
+
+
+function tablas() {
+  document.getElementById("tablaS").innerHTML = ``;
+  for (let i = 0; i < meses[0].teams.length; i++) {
+
+    var tableData = document.createElement("tr");
+    tableData.innerHTML = `
+            <td> ${meses[0].teams[i].vsName} </td>
+            <td> <a href=${meses[0].teams[i].map_iframe} target="blank" class="a"> ${meses[0].teams[i].location} </td>    
+            <td> ${meses[0].teams[i].times} </td>
+            <td> ${meses[0].teams[i].day} </td>
+        `
+    document.getElementById('tablaS').appendChild(tableData);
+
+    document.getElementById("tablaO").innerHTML = ``;
+    for (let i = 0; i < meses[1].teams.length; i++) {
+
+      var tableData = document.createElement("tr");
+      tableData.innerHTML = `
+              <td> ${meses[1].teams[i].vsName} </td>
+              <td> <a href=${meses[1].teams[i].map_iframe} target="blank" class="a"> ${meses[1].teams[i].location} </td>
+              <td> ${meses[1].teams[i].times} </td>
+              <td> ${meses[1].teams[i].day} </td>
+          `
+      document.getElementById('tablaO').appendChild(tableData);
+    }
+  }
+}
+tablas()
+
+
+/* function tablaT() {
+  document.getElementById("tablaT").innerHTML = ``;
+  for (let i = 0; i < meses[0].teams.length; i++) {
+
+    var tableData = document.createElement("tr");
+    tableData.innerHTML = `
+            <input type="button" value="${meses[0].teams[i].vsName}"> 
+        `
+    document.getElementById('tablaT').appendChild(tableData);
+  }
+}
+tablaT() */
+
+
+
+
+// var mesesS = meses[0].teams;
+// var mesesO = meses[1].teams;
+
+// let U1aU4 = [];
+// let equiposNombre = [];
+
+
+// for (let i = 0; i < mesesS.length; i++) {
+//   if (mesesS[i].vsName === "U1 and U4") {
+//     U1aU4.push(mesesS[i]);
+//   }
+// }
+// console.log(U1aU4);
+
+// for (let x = 0; x < mesesS.length; x++) {
+//   equiposNombre.push(mesesS[x].vsName)
+// }
+
+// for (let j = 0; j < mesesS.length; j++) {
+//   document.createElement
+
+// }
+
+
+// function createVariables() {
+//   var accounts = [];
+
+//   for (var i = 0; i <= 20; ++i) {
+//     accounts[i] = "1";
+//   }
+
+//   return accounts;
+// }
+// createVariables()
+
+
+function next() {
+  var firstRe = document.getElementById("firstRe");
+  var secondRe = document.getElementById("secondRe")
+
+  if (firstRe.style.display === "block") {
+    firstRe.style.display = "none";
+  }
+  else {
+    firstRe.style.display = "none"
+  }
+  next.then(secondRe.style.display = "block")
+}
+
+function next2() {
+  var secondRe = document.getElementById("secondRe")
+  var thirdRe = document.getElementById("thirdRe")
+
+  if (secondRe.style.display === "block") {
+    secondRe.style.display = "none";
+  }
+  else {
+    secondRe.style.display = "none"
+  } next.then(thirdRe.style.display = "block")
+}
+
+function next3() {
+  var thirdRe = document.getElementById("thirdRe")
+  var fourthRe = document.getElementById("fourthRe")
+
+  if (thirdRe.style.display === "block") {
+    thirdRe.style.display = "none";
+  }
+  else {
+    thirdRe.style.display = "none"
+  } next.then(fourthRe.style.display = "block")
+}
+
+function next4() {
+  var fourthRe = document.getElementById("fourthRe")
+  var fifthRe = document.getElementById("fifthRe")
+
+  if (fourthRe.style.display === "block") {
+    fourthRe.style.display = "none";
+  }
+  else {
+    fourthRe.style.display = "none"
+  } next.then(fifthRe.style.display = "block")
+}
+
+
+
+
+
+function back2() {
+  var firstRe = document.getElementById("firstRe");
+  var secondRe = document.getElementById("secondRe")
+
+  if (firstRe.style.display === "none") {
+    firstRe.style.display = "block";
+  }
+  else {
+    firstRe.style.display = "block"
+  }
+  next.then(secondRe.style.display = "none")
+}
+
+function back3() {
+  var secondRe = document.getElementById("secondRe")
+  var thirdRe = document.getElementById("thirdRe")
+
+  if (secondRe.style.display === "none") {
+    secondRe.style.display = "block";
+  }
+  else {
+    secondRe.style.display = "block"
+  } next.then(thirdRe.style.display = "none")
+}
+
+function back4() {
+  var thirdRe = document.getElementById("thirdRe")
+  var fourthRe = document.getElementById("fourthRe")
+
+  if (thirdRe.style.display === "none") {
+    thirdRe.style.display = "block";
+  }
+  else {
+    thirdRe.style.display = "block"
+  } next.then(fourthRe.style.display = "none")
+}
+
+function backFinal() {
+  var fourthRe = document.getElementById("fourthRe")
+  var fifthRe = document.getElementById("fifthRe")
+
+
+  if (fourthRe.style.display === "none") {
+    fourthRe.style.display = "block";
+  }
+  else {
+    fourthRe.style.display = "block"
+  } next.then(fifthRe.style.display = "none", display)
+}
